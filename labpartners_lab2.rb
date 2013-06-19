7bdb2a3100a79ccc0f71eea7fb4a05a8a050a655
Christopher Wright <cmwright33@gmail.com>
2013-06-19 17:39:17 -0400

    updated code on lappartners_lab and created new code on labpartners_lab2

--- labpartners_lab2.rb
+++ labpartners_lab2.rb
@@ -1,18 +1,14 @@
-
 students = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Christine Coulter",
 "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong",
 "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi",
 "Peter Lai", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "bart simpson"]
 
-
-
-
-number_of_students = students.length
+students.shuffle!
+num_students = students.length
 group_size = 3
-number_of_groups = number_of_students / group_size
 
-number_of_groups.times do |group|
-  puts students.pop(group_size).join(', ')
+
+
+num_students.times do |x|
+puts x.pop(3)
 end
-
-

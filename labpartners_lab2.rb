9964d53edbb6237e020a8a248bf58e3488fa6d6e
Christopher Wright <cmwright33@gmail.com>
2013-06-19 20:54:15 -0400

    added a shuffle, random group sizes and each slice to code

--- labpartners_lab2.rb
+++ labpartners_lab2.rb
@@ -1,14 +1,10 @@
+#require 'enumerator'
+
 students = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Christine Coulter",
 "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong",
 "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi",
 "Peter Lai", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "bart simpson"]
 
 students.shuffle!
-num_students = students.length
-group_size = 3
-
-
-
-num_students.times do |x|
-puts x.pop(3)
-end
+group_size = rand(0..27)
+students.each_slice(group_size){|slice| p slice}
